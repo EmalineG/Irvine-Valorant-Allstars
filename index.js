91b3f883b7731c31847182df298076b768d5b2ec
emalineg <mgayhart0@ivc.edu>
2022-12-09 03:01:01 -0800

    better

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// document.querySelector("#a").addEventListener('click', () => {
-//     window.open("https://instagram.com/irvinehighasb", '_blank');});
+document.querySelector("#a").addEventListener('click', () => {
+    window.open("https://instagram.com/irvinehighasb", '_blank');});
 
-// document.querySelector("#b").addEventListener('click', () => {
-//         window.open("https://instagram.com/nhsgamingclub", '_blank');});
+document.querySelector("#b").addEventListener('click', () => {
+        window.open("https://instagram.com/nhsgamingclub", '_blank');});
     
-// document.querySelector("#c").addEventListener('click', () => {
-//         window.open("https://instagram.com/whsesports", '_blank');});
+document.querySelector("#c").addEventListener('click', () => {
+        window.open("https://instagram.com/whsesports", '_blank');});
 
-// document.querySelector("#d").addEventListener('click', () => {
-//         window.open("https://instagram.com/uhstrojanarmy", '_blank');});
+document.querySelector("#d").addEventListener('click', () => {
+        window.open("https://instagram.com/uhstrojanarmy", '_blank');});
 
-// document.querySelector("#e").addEventListener('click', () => {
-//             window.open("https://instagram.com/portolahighesports", '_blank');});
+document.querySelector("#e").addEventListener('click', () => {
+            window.open("https://instagram.com/portolahighesports", '_blank');});
 const el = document.querySelector("#moveimg");
 
 el.addEventListener("mousemove", (e) => {
@@ -21,6 +21,18 @@ el.addEventListener("mousemove", (e) => {
 
 document.querySelector("#apply").addEventListener('click', () => {
     window.open("https://forms.gle/rT71c1bTeS1meUr98", '_blank');}
+);
+
+document.querySelector("#discord").addEventListener('click', () => {
+  window.open("https://discord.gg/YngTK56EYZ", '_blank');}
+);
+
+document.querySelector("#website").addEventListener('click', () => {
+  window.open("https://ivc.edu/esports", '_blank');}
+);
+
+document.querySelector("#highlights").addEventListener('click', () => {
+  window.open("https://www.twitch.tv/ivc_esports/clips?filter=clips&range=all", '_blank');}
 );
 
 
